73a9af29f6c1f4e274d297de09c2f5950ee163c2
Johannes Wienke <languitar@semipol.de>
2021-04-05 14:01:10 +0200

    Enable system-specific config

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -15,6 +15,10 @@ require('ide')
 require('utils')
 require('mappings')
 
+-- optional system-specific configuration
+-- pcall avoids errors if the config doesn't exist
+pcall(require, 'systemconfig')
+
 -- allow project-specific settings
 local local_vimrc = vim.fn.getcwd() .. '/.exrc'
 if vim.loop.fs_stat(local_vimrc) then
