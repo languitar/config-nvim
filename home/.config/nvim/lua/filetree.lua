21a44618c8157c4f7260849ef186c01a5ccef38c
Johannes Wienke <johannes.wienke@plan.one>
2021-04-14 15:44:04 +0200

    Use gitignore for nvim-tree

--- home/.config/nvim/lua/filetree.lua
+++ home/.config/nvim/lua/filetree.lua
@@ -1,6 +1,7 @@
 vim.g.nvim_tree_side = "left"
 vim.g.nvim_tree_width = 30
-vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache"}
+vim.g.nvim_tree_ignore = {".git", "node_modules", ".cache", "__pycache__"}
+vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_auto_open = 0
 vim.g.nvim_tree_auto_close = 0
 vim.g.nvim_tree_quit_on_open = 0
