7658ee6747105af7d2ece1e08fd7eaf646f2ad29
Johannes Wienke <languitar@semipol.de>
2021-10-23 21:24:40 +0200

    Add wilder

--- home/.config/nvim/lua/plugins.lua
+++ home/.config/nvim/lua/plugins.lua
@@ -53,6 +53,43 @@ return require('packer').startup(function()
   }
 
   -- utilities
+  use {
+    'gelguy/wilder.nvim',
+    requires = { 'romgrk/fzy-lua-native' },
+    config = function()
+      vim.api.nvim_exec([[
+        call wilder#setup({'modes': [':', '/', '?']})
+        call wilder#set_option('use_python_remote_plugin', 0)
+
+        call wilder#set_option('pipeline', [
+              \   wilder#branch(
+              \     wilder#cmdline_pipeline({
+              \       'fuzzy': 1,
+              \       'fuzzy_filter': wilder#lua_fzy_filter(),
+              \     }),
+              \     wilder#vim_search_pipeline(),
+              \   ),
+              \ ])
+
+        call wilder#set_option('renderer', wilder#renderer_mux({
+              \ ':': wilder#popupmenu_renderer({
+              \   'highlighter': wilder#lua_fzy_highlighter(),
+              \   'left': [
+              \     ' ',
+              \     wilder#popupmenu_devicons(),
+              \   ],
+              \   'right': [
+              \     ' ',
+              \     wilder#popupmenu_scrollbar(),
+              \   ],
+              \ }),
+              \ '/': wilder#wildmenu_renderer({
+              \   'highlighter': wilder#lua_fzy_highlighter(),
+              \ }),
+              \ }))
+      ]], false)
+    end
+  }
   use 'kana/vim-textobj-user'
   use 'glts/vim-textobj-comment'
   use 'michaeljsmith/vim-indent-object'
