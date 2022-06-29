1c98a440e746a9debd28e37135a45774b5e5b09f
Johannes Wienke <johannes.wienke@plan.one>
2022-06-29 17:07:34 +0200

    Remove an unused lua function

--- home/.config/nvim/lua/ide.lua
+++ home/.config/nvim/lua/ide.lua
@@ -7,9 +7,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 })
 
 local custom_attach = function(client, bufnr)
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
 	local function buf_set_option(...)
 		vim.api.nvim_buf_set_option(bufnr, ...)
 	end
