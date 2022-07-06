0d89f2acfb3db54f93e17e7db2e7640090273fbd
Johannes Wienke <johannes.wienke@plan.one>
2022-07-06 15:47:27 +0200

    Remove detectindent settings
    Plugin has been replaced.

--- home/.config/nvim/lua/utils.lua
+++ home/.config/nvim/lua/utils.lua
@@ -2,7 +2,3 @@
 vim.api.nvim_command([[
 autocmd TextYankPost * silent! lua vim.highlight.on_yank {timeout=500}
 ]])
-
--- detect indent settings
-vim.g.detectindent_preferred_expandtab = 1
-vim.g.detectindent_preferred_indent = 4
